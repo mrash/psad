6ac0ea88994ea6a12c4604cb0e56ffe82a888cc9
Michael Rash <mbr@cipherdyne.org>
2003-05-29 00:28:00 +0000

    -Stubbed in code to re-import config upon receiving a HUP signal. -Switched to diskmond.conf specific config file.
    git-svn-id: file:///home/mbr/svn/psad_repos/psad/trunk@702 91a0a83b-1414-0410-bf9a-c3dbc33e90b6

--- diskmond.pl
+++ diskmond.pl
@@ -37,7 +37,6 @@
 use lib '/usr/lib/psad';
 use Psad;
 use Sys::Hostname 'hostname';
-use File::stat;
 use File::Copy;
 use POSIX 'setsid';
 use Getopt::Long 'GetOptions';
@@ -45,100 +44,108 @@ use strict;
 
 ### establish the default path to the config file (can be
 ### over-ridden with the -c <file> command line option.
-my $CONFIG_FILE = '/etc/psad/psad.conf';
+my $CONFIG_FILE = '/etc/psad/diskmond.conf';
+
+### configuration hash
+my %config;
+
+### commands hash
+my %cmds;
+
+### flag used for HUP signal
+my $hup_flag = 0;
 
 ### handle command line arguments
-die " ** Specify the path to the psad.conf file with " . 
+die " ** Specify the path to the psad.conf file with " .
     "\"-c <file>\".\n\n" unless (GetOptions (
     'config=s' => \$CONFIG_FILE
 ));
 
-### read in the configuration file
-my ($Config_href, $Cmds_href) = &Psad::buildconf($CONFIG_FILE);
+### import config
+&import_config();
 
-### make sure the configuration is complete
-&check_config();
+#my $Archive_files_aref = $config{'ARCHIVE_FILES'};
 
-my %Config = %$Config_href;
-my %Cmds   = %$Cmds_href;
-#my $Archive_files_aref = $Config{'ARCHIVE_FILES'};
-
-### Check to make sure the commands specified in the config section
-### are in the right place, and attempt to correct automatically if not.
-&Psad::check_commands(\%Cmds);
-
-&Psad::unique_pid($Config{'DISKMOND_PID_FILE'});
+&Psad::unique_pid($config{'DISKMOND_PID_FILE'});
 
 ### install WARN and DIE handlers
 $SIG{'__WARN__'} = \&Psad::warn_handler;
 $SIG{'__DIE__'}  = \&Psad::die_handler;
 
+### install HUP handler so config can be re-imported
+$SIG{'HUP'}  = \&hup_sig;
+
 my $pid = fork;
 exit if $pid;
-die " ** $0: Couldn't fork: $!" unless defined($pid);
+die " ** $0: Couldn't fork: $!" unless defined $pid;
 POSIX::setsid() or die " ** $0: Can't start a new session: $!\n";
 
 ### write the pid to the pid file
-&Psad::writepid($Config{'DISKMOND_PID_FILE'});
+&Psad::writepid($config{'DISKMOND_PID_FILE'});
 
 ### initialize partition usage
 my $usage     = 0;
 my $email_ctr = 0;
 my $hostname  = hostname;
 
-my $config_mtime = stat($CONFIG_FILE)->mtime;
 #====================== main =======================
 ### main loop
 for (;;) {
-    ### See if we need to import any changed config variables
-    &check_import_config(\$config_mtime, $CONFIG_FILE);
 
-    $usage = &get_usage();
+    if ($hup_flag) {
+        ### clear the HUP flag and re-import the config
+        $hup_flag = 0;
+        &import_config();
+    }
+
+    $usage = &get_disk_usage();
     ### Check to see if we need to start archiving
-    if ($usage >= $Config{'MAX_DISK_PERCENTAGE'}) {
+    if ($usage >= $config{'MAX_DISK_PERCENTAGE'}) {
         &rm_data();
-        open DW, "> $Config{'PSAD_DIR'}/disk_limit";
-        print DW "psad: disk utilization above $Config{'MAX_DISK_PERCENTAGE'}%\n";
-        print DW "      on disk partition that holds $Config{'PSAD_DIR'}\n";
+        open DW, "> $config{'PSAD_DIR'}/disk_limit";
+        print DW "psad: disk utilization above ",
+            "$config{'MAX_DISK_PERCENTAGE'}%\n";
+        print DW "      on disk partition that holds ",
+            "$config{'PSAD_DIR'}\n";
         print DW "      diskmond removed all ip directories in " .
-            "$Config{'PSAD_DIR'}.\n";
+            "$config{'PSAD_DIR'}.\n";
         close DW;
-        &Psad::sendmail("psad: disk utilization above $Config{'MAX_DISK_PERCENTAGE'}%",
-            "$Config{'PSAD_DIR'}/disk_limit", $Config{'EMAIL_ADDRESSES'},
-            $Cmds{'mail'});
+        &Psad::sendmail(" ** psad diskmond: disk utilization above " .
+            "$config{'MAX_DISK_PERCENTAGE'}%", "$config{'PSAD_DIR'}/disk_limit",
+            $config{'EMAIL_ADDRESSES'}, $cmds{'mail'});
         $email_ctr++;
         if ($email_ctr > 50) {
-            &Psad::sendmail("psad: diskmond giving up, stopping psad.",
-                $Config{'EMAIL_ADDRESSES'}, $Cmds{'mail'});
+            &Psad::sendmail(" ** psad: diskmond giving up.  Stopping psad.",
+                $config{'EMAIL_ADDRESSES'}, $cmds{'mail'});
         }
     } else {
         $email_ctr = 0;
     }
     ### check disk usage every $CHECK_INTERVAL seconds
-    sleep $Config{'DISKMOND_CHECK_INTERVAL'};
+    sleep $config{'DISKMOND_CHECK_INTERVAL'};
 }
 exit 0;
 #===================== end main ====================
 
-sub get_usage() {
-    my @df_data = `$Cmds{'df'} $Config{'PSAD_DIR'}`;
+sub get_disk_usage() {
+    my @df_data = `$cmds{'df'} $config{'PSAD_DIR'}`;
     my ($prcnt) = ($df_data[$#df_data] =~ /(\d+)%/);
     return $prcnt;
 }
 
 sub rm_data() {
-    chdir $Config{'PSAD_DIR'} or die
-        " ** Could not chdir $Config{'PSAD_DIR'}: $!";
+    chdir $config{'PSAD_DIR'} or die
+        " ** Could not chdir $config{'PSAD_DIR'}: $!";
 
-    &rm_scanlog($Config{'PSAD_DIR'});
-    &rm_scanlog($Config{'SCAN_DATA_ARCHIVE_DIR'});
+    &rm_scanlog($config{'PSAD_DIR'});
+    &rm_scanlog($config{'SCAN_DATA_ARCHIVE_DIR'});
 
-    if (-e $Config{'FW_DATA_FILE'}) {
-        open F, "> $Config{'FW_DATA_FILE'}";
+    if (-e $config{'FW_DATA_FILE'}) {
+        open F, "> $config{'FW_DATA_FILE'}";
         close F;
     }
-    if (-e "$Config{'SCAN_DATA_ARCHIVE_DIR'}/fwdata_archive") {
-        open F, "> $Config{'SCAN_DATA_ARCHIVE_DIR'}/fwdata_archive";
+    if (-e "$config{'SCAN_DATA_ARCHIVE_DIR'}/fwdata_archive") {
+        open F, "> $config{'SCAN_DATA_ARCHIVE_DIR'}/fwdata_archive";
         close F;
     }
     return;
@@ -165,29 +172,31 @@ sub rm_scanlog() {
     return;
 }
 
-sub check_import_config() {
-    my ($mtime_ref, $file) = @_;
-    my $mtime_tmp = stat($file)->mtime;
-    if ($mtime_tmp != $$mtime_ref) {  ### the file was modified, so import
+sub import_config() {
+    ### read in the configuration file
+    &Psad::buildconf(\%config, \%cmds, $CONFIG_FILE);
 
-        ($Config_href, $Cmds_href) = &Psad::buildconf($file);
+    ### make sure the configuration is complete
+    &required_vars();
 
-        ### make sure the configuration is complete
-        &check_config();
+    ### Check to make sure the commands specified in the config section
+    ### are in the right place, and attempt to correct automatically if not.
+    &Psad::check_commands(\%cmds);
 
-        %Config = %$Config_href;
-        %Cmds   = %$Cmds_href;
-        $$mtime_ref = $mtime_tmp;
-    }
     return;
 }
 
-sub check_config() {
+sub required_vars() {
     my @required_vars = qw(
         FW_DATA DISKMOND_CHECK_INTERVAL
         MAX_DISK_PERCENTAGE DISKMOND_PID_FILE
         PSAD_DIR SCAN_DATA_ARCHIVE_DIR
     );
-    &Psad::defined_vars($CONFIG_FILE, \@required_vars, $Config_href);
+    &Psad::defined_vars($CONFIG_FILE, \@required_vars, \%config);
+    return;
+}
+
+sub hup_sig() {
+    $hup_flag = 1;
     return;
 }

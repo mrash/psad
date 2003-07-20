c5f53f44500bc1ad171757a85e5017f20d484bfc
Michael Rash <mbr@cipherdyne.org>
2003-07-20 13:04:06 +0000

    updated to whois-4.6.6
    git-svn-id: file:///home/mbr/svn/psad_repos/psad/trunk@767 91a0a83b-1414-0410-bf9a-c3dbc33e90b6

--- whois/whois.c
+++ whois/whois.c
@@ -191,6 +191,17 @@ int main(int argc, char *argv[])
 		printf(_("\nFound a referral to %s.\n\n"), server);
 		alarm(60);
 		break;
+	    case 9:
+		if (verb)
+		    puts(_("Connecting to whois.nic.cc."));
+		sockfd = openconn("whois.nic.cc", NULL);
+		server = query_crsnic(sockfd, qstring);
+		close(sockfd);
+		if (!server)
+		    exit(0);
+		printf(_("\nFound a referral to %s.\n\n"), server);
+		alarm(60);
+		break;
 	    case 7:
 		if (verb)
 		    puts(_("Connecting to whois.publicinterestregistry.net."));
@@ -318,19 +329,19 @@ const char *whichwhois(const char *s)
 	    for (i = 0; ip6_assign[i].serv; i++)
 		if (v6net == ip6_assign[i].net)
 		    return ip6_assign[i].serv;
-	    return "\006";			/* unknown allocation */
+	    return "\x06";			/* unknown allocation */
 	} else if (strncasecmp(s, "3ffe:", 5) == 0)
 	    return "whois.6bone.net";
 	/* RPSL hierarchical object like AS8627:fltr-TRANSIT-OUT */
 	else if (strncasecmp(s, "as", 2) == 0 && isasciidigit(s[2]))
 	    return whereas(atoi(s + 2));
 	else
-	    return "\005";
+	    return "\x05";
     }
 
     /* email address */
     if (strchr(s, '@'))
-	return "\005";
+	return "\x05";
 
     /* no dot and no hyphen means it's a NSI NIC handle or ASN (?) */
     if (!strpbrk(s, ".-")) {
@@ -345,7 +356,7 @@ const char *whichwhois(const char *s)
 	if (*s == '!')	/* NSI NIC handle */
 	    return "whois.networksolutions.com";
 	else
-	    return "\005";	/* probably a unknown kind of nic handle */
+	    return "\x05";	/* probably a unknown kind of nic handle */
     }
 
     /* smells like an IP? */
@@ -353,7 +364,7 @@ const char *whichwhois(const char *s)
 	for (i = 0; ip_assign[i].serv; i++)
 	    if ((ip & ip_assign[i].mask) == ip_assign[i].net)
 		return ip_assign[i].serv;
-	return "\005";			/* not in the unicast IPv4 space */
+	return "\x05";			/* not in the unicast IPv4 space */
     }
 
     /* check the TLDs list */
@@ -371,9 +382,9 @@ const char *whichwhois(const char *s)
 	return "";
     }
 
-    /* has dot and maybe a hypen and it's not in tld_serv[], WTF is it? */
+    /* has dot and maybe a hyphen and it's not in tld_serv[], WTF is it? */
     /* either a TLD or a NIC handle we don't know about yet */
-    return "\005";
+    return "\x05";
 }
 
 const char *whereas(const unsigned short asn)
@@ -383,7 +394,7 @@ const char *whereas(const unsigned short asn)
     for (i = 0; as_assign[i].serv; i++)
 	if (asn >= as_assign[i].first && asn <= as_assign[i].last)
 	    return as_assign[i].serv;
-    return "\006";
+    return "\x06";
 }
 
 char *queryformat(const char *server, const char *flags, const char *query)
@@ -417,12 +428,11 @@ char *queryformat(const char *server, const char *flags, const char *query)
 	    puts(_("Warning: RIPE flags used with a traditional server."));
 	strcat(buf, flags);
     }
-    if (!isripe && strcmp(server, "whois.nic.mil") == 0 &&
+    /* FIXME: /e is not applied to .JP ASN */
+    if (!isripe && (strcmp(server, "whois.nic.mil") == 0 ||
+	    strcmp(server, "whois.nic.ad.jp") == 0) &&
 	    strncasecmp(query, "AS", 2) == 0 && isasciidigit(query[2]))
 	sprintf(buf, "AS %s", query + 2);	/* fix query for DDN */
-    if (!isripe && strcmp(server, "whois.arin.net") == 0 &&
-	    strncasecmp(query, "AS", 2) == 0 && isasciidigit(query[2]))
-	sprintf(buf, "A %s", query + 2);	/* always ask for a ASN */
     else if (!isripe && strcmp(server, "whois.corenic.net") == 0)
 	sprintf(buf, "--machine %s", query);	/* machine readable output */
     else if (!isripe && strcmp(server, "whois.nic.ad.jp") == 0) {
@@ -510,6 +520,8 @@ const char *query_crsnic(const int sock, const char *query)
     if (write(sock, temp, strlen(temp)) < 0)
 	err_sys("write");
     while (fgets(buf, sizeof(buf), fi)) {
+	if (strncmp(buf, "No match for \"", 14) == 0)	/* ugly */
+	    fputs(buf, stdout);
 	/* If there are multiple matches only the server of the first record
 	   is queried */
 	if (state == 0 && strncmp(buf, "   Domain Name:", 15) == 0)
@@ -520,7 +532,8 @@ const char *query_crsnic(const int sock, const char *query)
 	    for (p = buf; *p != ':'; p++);	/* skip until colon */
 	    for (p++; *p == ' '; p++);		/* skip colon and spaces */
 	    ret = malloc(strlen(p) + 1);
-	    for (q = ret; *p != '\n' && *p != '\r'; *q++ = *p++); /*copy data*/
+	    for (q = ret; *p != '\n' && *p != '\r' && *p != ' '; *q++ = *p++)
+		; /*copy data*/
 	    *q = '\0';
 	    state = 2;
 	}
@@ -552,10 +565,10 @@ const char *query_pir(const int sock, const char *query)
 	/* If there are multiple matches only the server of the first record
 	   is queried */
 	if (state == 0 &&
-    strncmp(buf, "Registrant Name:CONTACT NOT AUTHORITATIVE", 15) == 0)
+    strncmp(buf, "Registrant Name:SEE SPONSORING REGISTRAR", 40) == 0)
 	    state = 1;
 	if (state == 1 &&
-    strncmp(buf, "Registrant Street1:Whois Server:", 16) == 0) {
+		strncmp(buf, "Registrant Street1:Whois Server:", 32) == 0) {
 	    char *p, *q;
 
 	    for (p = buf; *p != ':'; p++);	/* skip until colon */
@@ -566,8 +579,7 @@ const char *query_pir(const int sock, const char *query)
 	    *q = '\0';
 	    state = 2;
 	}
-	if (verb)
-	    fputs(buf, stdout);
+	fputs(buf, stdout);
     }
     if (ferror(fi))
 	err_sys("fgets");

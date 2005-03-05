b04be2f6db83359c18659c5385edb0e325c6cc35
Michael Rash <mbr@cipherdyne.org>
2005-03-05 14:32:04 +0000

    added call to find_ip_rule()
    git-svn-id: file:///home/mbr/svn/psad_repos/psad/trunk@1291 91a0a83b-1414-0410-bf9a-c3dbc33e90b6

--- chainmgr_test.pl
+++ chainmgr_test.pl
@@ -12,15 +12,22 @@ my $ipt = new IPTables::ChainMgr(
 my ($rv, $status_msg) = $ipt->create_chain('filter', 'PSAD');
 print "$status_msg: $rv\n";
 
+($rv, $status_msg) = $ipt->add_jump_rule('filter', 'INPUT', 'PSAD');
+print "$status_msg: $rv\n";
+
 ($rv, $status_msg) = $ipt->add_ip_rule('1.1.1.1', 'filter', 'PSAD', 'DROP');
 print "$status_msg: $rv\n";
+
+$rv = $ipt->find_ip_rule('1.1.1.1', '0.0.0.0/0', 'filter', 'PSAD', 'DROP');
+print "find ip: $rv\n";
+
 ($rv, $status_msg) = $ipt->add_ip_rule('1.1.1.1', 'filter', 'PSAD', 'DROP');
 print "$status_msg: $rv\n";
 
 ($rv, $status_msg) = $ipt->delete_ip_rule('1.1.1.1', 'filter', 'PSAD', 'DROP');
 print "$status_msg: $rv\n";
 
-($rv, $status_msg) = $ipt->delete_chain('filter', 'PSAD');
+($rv, $status_msg) = $ipt->delete_chain('filter', 'INPUT', 'PSAD');
 print "$status_msg: $rv\n";
 
 exit 0;

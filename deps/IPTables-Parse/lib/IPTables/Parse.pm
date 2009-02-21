30ebe9027756fcf11a28cf727063b85af1ec9f06
Michael Rash <mbr@cipherdyne.org>
2009-02-21 04:25:42 +0000

    - Updated IPTables::Parse to 0.7. - Updated IPTables::ChainMgr to 0.9.
    git-svn-id: file:///home/mbr/svn/psad_repos/psad/trunk@2252 91a0a83b-1414-0410-bf9a-c3dbc33e90b6

--- deps/IPTables-Parse/lib/IPTables/Parse.pm
+++ deps/IPTables-Parse/lib/IPTables/Parse.pm
@@ -7,7 +7,7 @@
 #
 # Author: Michael Rash (mbr@cipherdyne.org)
 #
-# Version: 0.5
+# Version: 0.7
 #
 ##################################################################
 #
@@ -17,19 +17,28 @@
 package IPTables::Parse;
 
 use 5.006;
+use POSIX ":sys_wait_h";
 use Carp;
 use strict;
 use warnings;
 use vars qw($VERSION);
 
-$VERSION = '0.5';
+$VERSION = '0.7';
 
 sub new() {
     my $class = shift;
     my %args  = @_;
 
     my $self = {
-        _iptables => $args{'iptables'} || '/sbin/iptables'
+        _iptables => $args{'iptables'} || '/sbin/iptables',
+        _iptout    => $args{'iptout'}    || '/tmp/ipt.out',
+        _ipterr    => $args{'ipterr'}    || '/tmp/ipt.err',
+        _ipt_alarm => $args{'ipt_alarm'} || 30,
+        _debug     => $args{'debug'}     || 0,
+        _verbose   => $args{'verbose'}   || 0,
+        _ipt_exec_style => $args{'ipt_exec_style'} || 'waitpid',
+        _ipt_exec_sleep => $args{'ipt_exec_sleep'} || 0,
+        _sigchld_handler => $args{'sigchld_handler'} || \&REAPER,
     };
     croak "[*] $self->{'_iptables'} incorrect path.\n"
         unless -e $self->{'_iptables'};
@@ -53,12 +62,9 @@ sub chain_policy() {
         @ipt_lines = <F>;
         close F;
     } else {
-        eval {
-            open IPT, "$iptables -t $table -n -L $chain -v |"
-                or croak "[*] Could not execute $iptables -t $table -n -L $chain -v";
-            @ipt_lines = <IPT>;
-            close IPT;
-        };
+        my ($rv, $out_ar, $err_ar) = $self->exec_iptables(
+                "$iptables -t $table -v -n -L $chain");
+        @ipt_lines = @$out_ar;
     }
 
     my $policy = '';
@@ -100,12 +106,9 @@ sub chain_rules() {
         @ipt_lines = <F>;
         close F;
     } else {
-        eval {
-            open IPT, "$iptables -t $table -n -L $chain -v |"
-                or croak "[*] Could not execute $iptables -t $table -n -L $chain -v";
-            @ipt_lines = <IPT>;
-            close IPT;
-        };
+        my ($rv, $out_ar, $err_ar) = $self->exec_iptables(
+                "$iptables -t $table -v -n -L $chain");
+        @ipt_lines = @$out_ar;
     }
 
     ### determine the output style (e.g. "-nL -v" or just plain "-nL"; if the
@@ -300,12 +303,9 @@ sub default_drop() {
         close F;
     } else {
 ### FIXME -v for interfaces?
-        eval {
-            open IPT, "$iptables -t $table -n -L $chain |"
-                or croak "[*] Could not execute $iptables -t $table -n -L $chain";
-            @ipt_lines = <IPT>;
-            close IPT;
-        };
+        my ($rv, $out_ar, $err_ar) = $self->exec_iptables(
+                "$iptables -t $table -n -L $chain");
+        @ipt_lines = @$out_ar;
     }
 
     return '[-] Could not get iptables output!', 0
@@ -390,12 +390,9 @@ sub default_log() {
         @ipt_lines = <F>;
         close F;
     } else {
-        eval {
-            open IPT, "$iptables -t $table -n -L |"
-                or croak "[*] Could not execute $iptables -t $table -n -L";
-            @ipt_lines = <IPT>;
-            close IPT;
-        };
+        my ($rv, $out_ar, $err_ar) = $self->exec_iptables(
+                "$iptables -t $table -n -L $chain");
+        @ipt_lines = @$out_ar;
     }
 
     ### determine the output style (e.g. "-nL -v" or just plain "-nL"; if the
@@ -508,6 +505,131 @@ sub sub_chains() {
     return;
 }
 
+sub exec_iptables() {
+    my $self  = shift;
+    my $cmd = shift || croak '[*] Must specify an iptables command to run.';
+    my $iptables  = $self->{'_iptables'};
+    my $iptout    = $self->{'_iptout'};
+    my $ipterr    = $self->{'_ipterr'};
+    my $debug     = $self->{'_debug'};
+    my $ipt_alarm = $self->{'_ipt_alarm'};
+    my $verbose   = $self->{'_verbose'};
+    my $ipt_exec_style = $self->{'_ipt_exec_style'};
+    my $ipt_exec_sleep = $self->{'_ipt_exec_sleep'};
+    my $sigchld_handler = $self->{'_sigchld_handler'};
+
+    croak "[*] $cmd does not look like an iptables command."
+        unless $cmd =~ m|^\s*iptables| or $cmd =~ m|^\S+/iptables|;
+
+    my $rv = 1;
+    my @stdout = ();
+    my @stderr = ();
+
+    my $fh = *STDERR;
+    $fh = *STDOUT if $verbose;
+
+    if ($debug or $verbose) {
+        print $fh localtime() . " [+] IPTables::Parse::",
+            "exec_iptables(${ipt_exec_style}()) $cmd\n";
+        if ($ipt_exec_sleep > 0) {
+            print $fh localtime() . " [+] IPTables::Parse::",
+                "exec_iptables() sleep seconds: $ipt_exec_sleep\n";
+        }
+    }
+
+    if ($ipt_exec_sleep > 0) {
+        if ($debug or $verbose) {
+            print $fh localtime() . " [+] IPTables::Parse: ",
+                "sleeping for $ipt_exec_sleep seconds before ",
+                "executing iptables command.\n";
+        }
+        sleep $ipt_exec_sleep;
+    }
+
+    if ($ipt_exec_style eq 'system') {
+        system qq{$cmd > $iptout 2> $ipterr};
+    } elsif ($ipt_exec_style eq 'popen') {
+        open CMD, "$cmd 2> $ipterr |" or croak "[*] Could not execute $cmd: $!";
+        @stdout = <CMD>;
+        close CMD;
+        open F, "> $iptout" or croak "[*] Could not open $iptout: $!";
+        print F for @stdout;
+        close F;
+    } else {
+        my $ipt_pid;
+
+        if ($debug or $verbose) {
+            print $fh localtime() . " [+] IPTables::Parse: " .
+                "Setting SIGCHLD handler to: " . $sigchld_handler . "\n";
+        }
+
+        local $SIG{'CHLD'} = $sigchld_handler;
+        if ($ipt_pid = fork()) {
+            eval {
+                ### iptables should never take longer than 30 seconds to execute,
+                ### unless there is some absolutely enormous policy or the kernel
+                ### is exceedingly busy
+                local $SIG{'ALRM'} = sub {die "[*] iptables command timeout.\n"};
+                alarm $ipt_alarm;
+                waitpid($ipt_pid, 0);
+                alarm 0;
+            };
+            if ($@) {
+                kill 9, $ipt_pid unless kill 15, $ipt_pid;
+            }
+        } else {
+            croak "[*] Could not fork iptables: $!"
+                unless defined $ipt_pid;
+
+            ### exec the iptables command and preserve stdout and stderr
+            exec qq{$cmd > $iptout 2> $ipterr};
+        }
+    }
+
+    if (-e $iptout) {
+        open F, "< $iptout" or croak "[*] Could not open $iptout";
+        @stdout = <F>;
+        close F;
+    }
+    if (-e $ipterr) {
+        open F, "< $ipterr" or croak "[*] Could not open $ipterr";
+        @stderr = <F>;
+        close F;
+
+        $rv = 0 if @stderr;
+    }
+
+    if ($debug or $verbose) {
+        print $fh localtime() . "     iptables command stdout:\n";
+        for my $line (@stdout) {
+            if ($line =~ /\n$/) {
+                print $fh $line;
+            } else {
+                print $fh $line, "\n";
+            }
+        }
+        print $fh localtime() . "     iptables command stderr:\n";
+        for my $line (@stderr) {
+            if ($line =~ /\n$/) {
+                print $fh $line;
+            } else {
+                print $fh $line, "\n";
+            }
+        }
+    }
+
+    return $rv, \@stdout, \@stderr;
+}
+
+sub REAPER {
+    my $stiff;
+    while(($stiff = waitpid(-1,WNOHANG))>0){
+        # do something with $stiff if you want
+    }
+    local $SIG{'CHLD'} = \&REAPER;
+    return;
+}
+
 1;
 __END__
 
@@ -517,19 +639,111 @@ IPTables::Parse - Perl extension for parsing iptables firewall rulesets
 
 =head1 SYNOPSIS
 
-    use IPTables::Parse;
+  use IPTables::Parse;
 
-    my $table = 'filter';
-    my $chain = 'INPUT';
-    if (&IPTables::Parse::default_drop($table, $chain)) {
-        print "[+] Table: $table, chain: $chain has a default rule\n";
-    } else {
-        print "[-] No default drop rule in table: $table, chain: $chain.\n";
-    }
+  my %opts = (
+      'iptables' => '/sbin/iptables',
+      'iptout'   => '/tmp/iptables.out',
+      'ipterr'   => '/tmp/iptables.err',
+      'debug'    => 0,
+      'verbose'  => 0
+  );
+
+  my $ipt_obj = new IPTables::Parse(%opts)
+      or die "[*] Could not acquire IPTables::Parse object";
+
+  my $rv = 0;
+
+  my $table = 'filter';
+  my $chain = 'INPUT';
+
+  my ($ipt_hr, $rv) = $ipt_obj->default_drop($table, $chain);
+  if ($rv) {
+      if (defined $ipt_hr->{'all'}) {
+          print "The INPUT chain has a default DROP rule for all protocols.\n";
+      } else {
+          for my $proto qw/tcp udp icmp/ {
+              if (defined $ipt_hr->{$proto}) {
+                  print "The INPUT chain drops $proto by default.\n";
+              }
+          }
+      }
+  } else {
+      print "[-] Could not parse iptables policy\n";
+  }
+
+  ($ipt_hr, $rv) = $ipt_obj->default_log($table, $chain);
+  if ($rv) {
+      if (defined $ipt_hr->{'all'}) {
+          print "The INPUT chain has a default LOG rule for all protocols.\n";
+      } else {
+          for my $proto qw/tcp udp icmp/ {
+              if (defined $ipt_hr->{$proto}) {
+                  print "The INPUT chain logs $proto by default.\n";
+              }
+          }
+      }
+  } else {
+      print "[-] Could not parse iptables policy\n";
+  }
 
 =head1 DESCRIPTION
 
-IPTables::Parse provides a perl module interface to parse iptables rulesets.
+The C<IPTables::Parse> package provides an interface to parse iptables
+rules on Linux systems through the direct execution of iptables commands, or
+from parsing a file that contains an iptables policy listing.  You can get the
+current policy applied to a table/chain, look for a specific user-defined chain,
+check for a default DROP policy, or determing whether or not logging rules exist.
+
+=head1 FUNCTIONS
+
+The IPTables::Parse extension provides an object interface to the following
+functions:
+
+=over 4
+
+=item chain_policy($table, $chain)
+
+This function returns the policy (e.g. 'DROP', 'ACCEPT', etc.) for the specified
+table and chain:
+
+  print "INPUT policy: ", $ipt_obj->chain_policy('filter', 'INPUT'), "\n";
+
+=item chain_rules($table, $chain)
+
+This function parses the specified chain and table and returns an array reference
+for all rules in the chain.  Each element in the array reference is a hash with
+the following keys (that contain values depending on the rule): C<src>, C<dst>,
+C<protocol>, C<s_port>, C<d_port>, C<target>, C<packets>, C<bytes>, C<intf_in>,
+C<intf_out>, C<to_ip>, C<to_port>, C<state>, C<raw>, and C<extended>.  The C<extended>
+element contains the rule output past the protocol information, and the C<raw>
+element contains the complete rule itself as reported by iptables.
+
+=item default_drop($table, $chain)
+
+This function parses the running iptables policy in order to determine if
+the specified chain contains a default DROP rule.  Two values are returned,
+a hash reference whose keys are the protocols that are dropped by default
+if a global ACCEPT rule has not accepted matching packets first, along with
+a return value that tells the caller if parsing the iptables policy was
+successful.  Note that if all protocols are dropped by default, then the
+hash key 'all' will be defined.
+
+  ($ipt_hr, $rv) = $ipt_obj->default_drop('filter', 'INPUT');
+
+=item default_log($table, $chain)
+
+This function parses the running iptables policy in order to determine if
+the specified chain contains a default LOG rule.  Two values are returned,
+a hash reference whose keys are the protocols that are logged by default
+if a global ACCEPT rule has not accepted matching packets first, along with
+a return value that tells the caller if parsing the iptables policy was
+successful.  Note that if all protocols are logged by default, then the
+hash key 'all' will be defined.  An example invocation is:
+
+  ($ipt_hr, $rv) = $ipt_obj->default_log('filter', 'INPUT');
+
+=back
 
 =head1 AUTHOR
 
@@ -537,6 +751,42 @@ Michael Rash, E<lt>mbr@cipherdyne.orgE<gt>
 
 =head1 SEE ALSO
 
-L<perl>.
+The IPTables::Parse is used by the IPTables::ChainMgr extension in support of
+the psad, fwsnort, and fwknop projects to parse iptables policies (see the psad(8),
+fwsnort(8), and fwknop(8) man pages).  As always, the iptables(8) provides the
+best information on command line execution and theory behind iptables.
+
+Although there is no mailing that is devoted specifically to the IPTables::Parse
+extension, questions about the extension will be answered on the following
+lists:
+
+  The psad mailing list: http://lists.sourceforge.net/lists/listinfo/psad-discuss
+  The fwknop mailing list: http://lists.sourceforge.net/lists/listinfo/fwknop-discuss
+  The fwsnort mailing list: http://lists.sourceforge.net/lists/listinfo/fwsnort-discuss
+
+The latest version of the IPTables::Parse extension can be found at:
+
+http://www.cipherdyne.org/modules/
+
+=head1 CREDITS
+
+Thanks to the following people:
+
+  Franck Joncourt <franck.mail@dthconnex.com>
+  Grant Ferley
+
+=head1 AUTHOR
+
+The IPTables::Parse extension was written by Michael Rash F<E<lt>mbr@cipherdyne.orgE<gt>>
+to support the psad, fwknop, and fwsnort projects.  Please send email to
+this address if there are any questions, comments, or bug reports.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2005-2008 by Michael Rash
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.5 or,
+at your option, any later version of Perl 5 you may have available.
 
 =cut

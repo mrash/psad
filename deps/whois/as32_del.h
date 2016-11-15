37a3ef06b0ba4cf4f358834967850b802109a962
Michael Rash <mbr@cipherdyne.org>
2016-11-14 22:14:24 -0500

    update whois files after build

--- deps/whois/as32_del.h
+++ deps/whois/as32_del.h
@@ -1,5 +1,11 @@
-{ 131072, 196607,	"whois.apnic.net" },	/* 2.0 2.65535 */
-{ 196608, 262143,	"whois.ripe.net" },	/* 3.0 3.65535 */
-{ 262144, 327679,	"whois.lacnic.net" },	/* 4.0 4.65535 */
-{ 327680, 393215,	"whois.afrinic.net" },	/* 5.0 5.65535 */
-{ 393216, 458751,	"whois.arin.net" },	/* 6.0 6.65535 */
+{ 131077u, 131086u,	"whois.nic.ad.jp" },
+{ 131092u, 131101u,	"whois.nic.or.kr" },
+{ 131152u, 131161u,	"whois.nic.ad.jp" },
+{ 131791u, 131890u,	"whois.nic.or.kr" },
+{ 131893u, 131992u,	"whois.nic.ad.jp" },
+{ 131072u, 196607u,	"whois.apnic.net" },	/* 2.0 2.65535 */
+{ 196608u, 262143u,	"whois.ripe.net" },	/* 3.0 3.65535 */
+{ 262144u, 327679u,	"whois.lacnic.net" },	/* 4.0 4.65535 */
+{ 327680u, 393215u,	"whois.afrinic.net" },	/* 5.0 5.65535 */
+{ 393216u, 458751u,	"whois.arin.net" },	/* 6.0 6.65535 */
+{ 4200000000u, 4294967294u,	"whois.ripe.net" },

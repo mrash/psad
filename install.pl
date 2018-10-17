89bdffb4390ea426cdeee640fb5453cbafcc5f90
Michael Rash <mbr@cipherdyne.org>
2018-10-16 17:27:36 -0700

    replace killall with pkill, fixes #45

--- install.pl
+++ install.pl
@@ -11,7 +11,7 @@
 #
 # Credits:  (see the CREDITS file)
 #
-# Copyright (C) 1999-2017 Michael Rash (mbr@cipherdyne.org)
+# Copyright (C) 1999-2018 Michael Rash (mbr@cipherdyne.org)
 #
 # License (GNU Public License):
 #
@@ -74,6 +74,7 @@ my %file_vars = (
 
 my %exclude_cmds = (
     'wget'         => '',
+    'netstat'      => '',
     'mail'         => '',
     'sendmail'     => '',
     'uname'        => '',
@@ -162,7 +163,6 @@ my $noarchive   = 0;
 my $uninstall   = 0;
 my $help        = 0;
 my $archived_old = 0;
-my $skip_syslog_test = 0;
 my $use_answers = 0;
 my $no_write_answers = 0;
 my %answers = ();
@@ -201,7 +201,6 @@ Getopt::Long::Configure('no_ignore_case');
     'no-rm-lib-dir'     => \$no_rm_old_lib_dir, ### remove any old /usr/lib/psad dir
     'no-preserve'       => \$noarchive,   ### Don't preserve existing configs.
     'syslog-conf=s'     => \$syslog_conf, ### specify path to syslog config file.
-    'no-syslog-test'    => \$skip_syslog_test,
     'uninstall'         => \$uninstall,   ### Uninstall psad.
     'init-dir=s'        => \$init_dir,
     'systemd-init-dir=s' => \$systemd_init_dir,
@@ -667,7 +666,7 @@ sub install() {
             if ($syslog_str eq 'syslogd') {
                 if (-e $syslog_conf) {
                     &append_fifo_syslog($syslog_conf);
-                    if (&run_cmd("$cmds{'killall'} -HUP syslogd 2> /dev/null") == 0) {
+                    if (&run_cmd("$cmds{'pkill'} --signal HUP syslogd 2> /dev/null") == 0) {
                         &logr("[+] HUP signal sent to syslogd.\n");
                         $restarted_syslog = 1;
                     }
@@ -675,7 +674,7 @@ sub install() {
             } elsif ($syslog_str eq 'rsyslogd') {
                 if (-e $syslog_conf) {
                     &append_fifo_syslog($syslog_conf);
-                    if (&run_cmd("$cmds{'killall'} -HUP rsyslogd 2> /dev/null") == 0) {
+                    if (&run_cmd("$cmds{'pkill'} --signal HUP rsyslogd 2> /dev/null") == 0) {
                         &logr("[+] HUP signal sent to rsyslogd.\n");
                         $restarted_syslog = 1;
                     }
@@ -684,7 +683,7 @@ sub install() {
             } elsif ($syslog_str eq 'syslog-ng') {
                 if (-e $syslog_conf) {
                     &append_fifo_syslog_ng($syslog_conf);
-                    if (&run_cmd("$cmds{'killall'} -HUP syslog-ng 2> /dev/null") == 0) {
+                    if (&run_cmd("$cmds{'pkill'} --signal HUP syslog-ng 2> /dev/null") == 0) {
                         &logr("[+] HUP signal sent to syslog-ng.\n");
                         $restarted_syslog = 1;
                     }
@@ -701,56 +700,6 @@ sub install() {
 
             unless ($restarted_syslog) {
                 &logr("[-] Could not restart any syslog daemons.\n");
-            }
-        }
-
-        if (-x $cmds{'iptables'} and not $skip_syslog_test) {
-            &logr("[+] Found iptables. Testing syslog configuration:\n");
-            ### make sure we actually see packets being logged by
-            ### the firewall.
-            if ($syslog_str ne 'ulogd') {
-                if (&test_syslog_config($syslog_str)) {
-                    &logr("[+] Successful $syslog_str reconfiguration.\n\n");
-                } else {
-                    if (&query_init_script_restart_syslog()) {
-
-                        my $restarted = 0;
-                        if ($syslog_str eq 'syslog-ng') {
-                            if (-e "$init_dir/syslog-ng") {
-                                &run_cmd("$init_dir/syslog-ng restart");
-                                $restarted = 1;
-                            }
-                        } elsif ($syslog_str eq 'rsyslogd') {
-                            if (-e "$init_dir/sysklogd") {
-                                &run_cmd("$init_dir/sysklogd restart");
-                                $restarted = 1;
-                            } elsif (-e "$init_dir/syslog") {
-                                &run_cmd("$init_dir/syslog restart");
-                                $restarted = 1;
-                            }
-                        } else {
-                            if (-e "$init_dir/rsyslog") {
-                                &run_cmd("$init_dir/rsyslog restart");
-                                $restarted = 1;
-                            }
-                        }
-                        ### test syslog config again now that we
-                        ### have restarted syslog via the init script
-                        ### instead of relying on a HUP signal to
-                        ### syslog
-                        if ($restarted) {
-                            if (&test_syslog_config($syslog_str)) {
-                                &logr("[+] Successful $syslog_str reconfiguration.\n\n");
-                            } else {
-                                &logr("[-] Unsuccessful $syslog_str reconfiguration.\n");
-                                &logr("    Consult the psad man page for the basic " .
-                                    "$syslog_str requirement to get psad to work.\n\n");
-                            }
-                        }
-                    } else {
-                        &logr("[-] Ok, hoping that psad can get packet data anyway.\n");
-                    }
-                }
             }
         }
     }
@@ -860,6 +809,8 @@ sub import_config() {
     &expand_vars();
 
     &required_vars();
+
+    $exclude_cmds{'ifconfig'} = '' if $config{'IFCFGTYPE'} =~ /iproute2/i;
 
     return;
 }
@@ -1154,6 +1105,26 @@ sub run_cmd() {
     return (system $cmd) >> 8;
 }
 
+sub get_netaddr() {
+    my ($ip, $mask) = @_;
+
+    my @ipbytes = split /\./, $ip;
+    my @mbytes  = split /\./, $mask;
+
+    my $netaddr = '';
+    for (my $i=0; $i < 4; $i++) {
+        my $byte1 = $mbytes[$i]+0;
+        my $byte2 = $ipbytes[$i]+0;
+        my $netaddr_byte = $byte1 & $byte2;
+        if ($i != 0) {
+            $netaddr = $netaddr . '.' . $netaddr_byte;
+        } else {
+            $netaddr = $netaddr_byte;
+        }
+    }
+    return $netaddr;
+}
+
 sub set_home_net() {
     my $file = shift;
 
@@ -1175,46 +1146,53 @@ sub set_home_net() {
 
     ### get all interfaces; even those that are down since they may
     ### brought up any time.
-    my @ifconfig_out = `$cmds{'ifconfig'} -a`;
     my $home_net_str = '';
     my $intf_name = '';
     my $net_ctr = 0;
     my %connected_subnets;
-    for my $line (@ifconfig_out) {
-        if ($line =~ /^\s*lo\s+Link/) {
-            $intf_name = '';
-            next;
-        }
-        if ($line =~ /^\s*dummy.*\s+Link/) {
-            $intf_name = '';
-            next;
-        }
-        if ($line =~ /^(\S+)\s+Link/) {
-            $intf_name = $1;
-            next;
-        }
-        if ($intf_name and
-                $line =~ /^\s+inet\s+.*?:($ip_re).*:($ip_re)/) {
-            my $ip = $1;
-            my $mask = $2;
 
-            my @ipbytes = split /\./, $ip;
-            my @mbytes  = split /\./, $mask;
-
-            my $netaddr = '';
-            for (my $i=0; $i < 4; $i++) {
-                my $byte1 = $mbytes[$i]+0;
-                my $byte2 = $ipbytes[$i]+0;
-                my $netaddr_byte = $byte1 & $byte2;
-                if ($i != 0) {
-                    $netaddr = $netaddr . '.' . $netaddr_byte;
-                } else {
-                    $netaddr = $netaddr_byte;
-                }
+    if ($config{'IFCFGTYPE'} =~ /iproute2/i) {
+        my @ip_out = `$cmds{'ip'} addr`;
+        for my $line (@ip_out) {
+            if ($line =~ /^\d+:\s+(\S+): </) {
+                $intf_name = $1;
+                next;
             }
+            if ($line =~ /^\s+inet\s+(\S+)\/(\d+)/) {
+                my $ip = $1;
+                my $mask = $2;
 
-            $connected_subnets{$intf_name} = "$netaddr/$mask";
-            $net_ctr++;
+                my $netaddr = &get_netaddr($ip, $mask);
+
+                $connected_subnets{$intf_name} = "$netaddr/$mask";
+                $net_ctr++;
+            }
+        }
+    } else {
+        my @ifconfig_out = `$cmds{'ifconfig'} -a`;
+        for my $line (@ifconfig_out) {
+            if ($line =~ /^\s*lo\s+Link/) {
+                $intf_name = '';
+                next;
+            }
+            if ($line =~ /^\s*dummy.*\s+Link/) {
+                $intf_name = '';
+                next;
+            }
+            if ($line =~ /^(\S+)\s+Link/) {
+                $intf_name = $1;
+                next;
+            }
+            if ($intf_name and
+                    $line =~ /^\s+inet\s+.*?:($ip_re).*:($ip_re)/) {
+                my $ip = $1;
+                my $mask = $2;
+
+                my $netaddr = &get_netaddr($ip, $mask);
+
+                $connected_subnets{$intf_name} = "$netaddr/$mask";
+                $net_ctr++;
+            }
         }
     }
     ### found two or more subnets, so forwarding traffic becomes
@@ -1612,156 +1590,6 @@ sub append_fifo_syslog() {
         ### reinstate kernel logging to our named pipe
         print SYSLOG "kern.info\t\t|$config{'PSAD_FIFO_FILE'}\n";
         close SYSLOG;
-    }
-    return;
-}
-
-sub test_syslog_config() {
-    my $syslog_str = shift;
-    my %used_ports;
-
-    ### first find an unused high tcp port to use for testing
-    my @netstat_out = `$cmds{'netstat'} -an`;
-
-    for my $line (@netstat_out) {
-        chomp $line;
-        if ($line =~ m/^\s*tcp\s+\d+\s+\d+\s+\S+:(\d+)\s/) {
-            ### $1 == protocol (tcp/udp), $2 == port number
-            $used_ports{$1} = '';
-        }
-    }
-
-    ### get the first unused high tcp port greater than 5000
-    my $test_port = 5000;
-    $test_port++ while defined $used_ports{$test_port};
-
-    ### make sure the interface is actually up
-    unless (&run_cmd("$cmds{'ifconfig'} lo up")) {
-        &logr("[-] Could not bring up the loopback interface.\n" .
-            "    Hoping the syslog reconfig will work anyway.\n");
-        return 0;
-    }
-
-    ### make sure we can see the loopback interface with
-    ### ifconfig
-    my @if_out = `$cmds{'ifconfig'} lo`;
-
-    unless (@if_out) {
-        &logr("[-] Could not see the loopback interface with ifconfig.  " .
-            "Hoping\n    syslog reconfig will work anyway.\n");
-        return 0;
-    }
-
-    my $lo_ip = '127.0.0.1';
-    my $found_ip = 0;
-    for my $line (@if_out) {
-        if ($line =~ /inet\s+addr:($ip_re)\s/) {
-            $lo_ip = $1;  ### this should always be 127.0.0.1
-            &logr("[-] loopback interface IP is not 127.0.0.1.  Continuing ".
-                "anyway.\n") unless $lo_ip eq '127.0.0.1';
-            $found_ip = 1;
-        }
-    }
-
-    unless ($found_ip) {
-        &logr("[-] The loopback interface does not have an IP.\n" .
-            "    Hoping the syslog reconfig will work anyway.\n");
-        return 0;
-    }
-
-    ### remove any "test_DROP" lines from fwdata file and ipt_prefix_ctr
-    ### before seeing if new ones can be written
-    &scrub_prefix_ctr();
-
-    my $start_kmsgsd = 1;
-    if (-e $config{'KMSGSD_PID_FILE'}) {
-        open PID, "< $config{'KMSGSD_PID_FILE'}" or die "[*] Could not open ",
-            "$config{'KMSGSD_PID_FILE'}: $!";
-        my $pid = <PID>;
-        close PID;
-        chomp $pid;
-        if (kill 0, $pid) {  ### kmsgsd is already running
-            $start_kmsgsd = 0;
-        }
-    }
-    if ($start_kmsgsd) {
-        ### briefly start kmsgsd just long enough to test syslog
-        ### with a packet to port 5000 (or higher).
-        unless (&run_cmd("${USRSBIN_DIR}/kmsgsd") == 0) {
-            &logr("[-] Could not start kmsgsd to test syslog.\n" .
-                "    Send email to Michael Rash (mbr\@cipherdyne.org)\n");
-            return 0;
-        }
-    }
-
-    ### insert a rule to deny traffic to the loopback
-    ### interface on $test_port
-    &run_cmd("$cmds{'iptables'} -I INPUT 1 -i lo -p tcp --dport " .
-        "$test_port -j LOG --log-prefix \"test_DROP \"");
-
-    open FWDATA, "$config{'FW_DATA_FILE'}" or
-        die "[*] Could not open $config{'FW_DATA_FILE'}: $!";
-
-    seek FWDATA,0,2;  ### seek to the end of the file
-
-    ### try to connect to $test_port to generate an iptables
-    ### drop message.  Note that since nothing is listening on
-    ### the port we will immediately receive a tcp reset.
-    my $sock = new IO::Socket::INET(
-        'PeerAddr' => $lo_ip,
-        'PeerPort' => $test_port,
-        'Proto'    => 'tcp',
-        'Timeout'  => 1
-    );
-    undef $sock if defined $sock;
-
-    ### sleep to give kmsgsd a chance to pick up the packet
-    ### log message from syslog
-    sleep 2;
-
-    my $found = 0;
-
-    my @pkts = <FWDATA>;
-    close FWDATA;
-    for my $pkt (@pkts) {
-        $found = 1 if $pkt =~ /test_DROP/;
-    }
-
-    ### remove the testing firewall rule
-    &run_cmd("$cmds{'iptables'} -D INPUT 1");
-
-    ### remove the any new test_DROP lines we just created
-    ### (this probably is not necessary because psad is not
-    ### running).
-    &scrub_prefix_ctr();
-
-    if ($found) {
-    } else {
-    }
-
-    if ($start_kmsgsd && -e $config{'KMSGSD_PID_FILE'}) {
-        open PID, "$config{'KMSGSD_PID_FILE'}" or return 0;
-        my $pid = <PID>;
-        close PID;
-        chomp $pid;
-        kill 9, $pid if kill 0, $pid;
-    }
-    return $found;
-}
-
-sub scrub_prefix_ctr() {
-    if (-e $config{'IPT_PREFIX_COUNTER_FILE'}) {
-        open SCRUB, "< $config{'IPT_PREFIX_COUNTER_FILE'}" or
-            die "[*] Could not open $config{'IPT_PREFIX_COUNTER_FILE'}: $!";
-        my @lines = <SCRUB>;
-        close SCRUB;
-
-        open SCRUB, "> $config{'IPT_PREFIX_COUNTER_FILE'}" or
-            die "[*] Could not open $config{'IPT_PREFIX_COUNTER_FILE'}: $!";
-        for my $line (@lines) {
-            print SCRUB $line unless $line =~ /test_DROP/;
-        }
-        close SCRUB;
     }
     return;
 }
